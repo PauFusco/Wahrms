bc2115393bf332e7a065bcb8c7ee37d1fd25308b
PauFusco <99730051+PauFusco@users.noreply.github.com>
2022-12-19 22:00:29 +0100

    physics started

--- Physics II - 2D Template/ModulePhysics.h
+++ Physics II - 2D Template/ModulePhysics.h
@@ -2,6 +2,61 @@
 #include "Module.h"
 #include "Globals.h"
 
+// Define Physics Globals here
+
+
+// b2Vec2 equivalent
+class wVec2 {
+public:
+	wVec2();
+	~wVec2();
+	
+	wVec2(float x, float y) : x(x), y(y) {}
+public:
+	float x, y;
+};
+
+// This defines whether the bodies will react
+// to collisions with movementor they will stay static
+enum bodyType
+{
+	DYNAMIC,
+	STATIC,
+	UNKNOWN
+};
+
+enum class ColliderType
+{
+	PLAYER,
+	FLOOR,
+	BUMPER,
+	AIR, // If we make a static square
+		 //	to occupy everything and make
+		 // it slow with air friction
+	UNKNOWN
+};
+
+class wBody
+{
+public:
+	wBody();
+	~wBody();
+
+	
+	void SetLinearVelocity(wVec2 v);
+	void SetPosititon(const wVec2& position);
+	void GetPosition(int& x, int& y) const;
+	
+public:
+	int width, height;
+	ColliderType ctype;
+
+private:
+	float mass, x, y;
+	wVec2 speed;
+};
+
+
 class ModulePhysics : public Module
 {
 public:
